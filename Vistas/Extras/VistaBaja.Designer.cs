10b6cc174ea5bea1671c9066be2b9a1ef124a968
Alexandro-dev <mercadogarciaalexandro10@gmail.com>
2026-04-09 20:03:44 -0500

    Flujo de vida de un articulo FINALIZADO
    Ahora los artículos pueden ser ingresados, vendidos, asignados y devueltos (queda pendiente la devolución por venta a plazos)

---
+++ Vistas/Extras/VistaBaja.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace ControlInventario.Vistas.Extras
+{
+    partial class VistaBaja
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TxtMotivo = new System.Windows.Forms.TextBox();
+            this.LblMensaje = new System.Windows.Forms.Label();
+            this.BtnAceptar = new System.Windows.Forms.Button();
+            this.BtnCancelar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // TxtMotivo
+            // 
+            this.TxtMotivo.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.TxtMotivo.Location = new System.Drawing.Point(37, 50);
+            this.TxtMotivo.Multiline = true;
+            this.TxtMotivo.Name = "TxtMotivo";
+            this.TxtMotivo.Size = new System.Drawing.Size(354, 189);
+            this.TxtMotivo.TabIndex = 0;
+            // 
+            // LblMensaje
+            // 
+            this.LblMensaje.AutoSize = true;
+            this.LblMensaje.Location = new System.Drawing.Point(28, 27);
+            this.LblMensaje.Name = "LblMensaje";
+            this.LblMensaje.Size = new System.Drawing.Size(374, 20);
+            this.LblMensaje.TabIndex = 1;
+            this.LblMensaje.Text = "Ingrese el motivo de la baja para el articulo *Codigo*";
+            // 
+            // BtnAceptar
+            // 
+            this.BtnAceptar.Location = new System.Drawing.Point(102, 258);
+            this.BtnAceptar.Name = "BtnAceptar";
+            this.BtnAceptar.Size = new System.Drawing.Size(87, 27);
+            this.BtnAceptar.TabIndex = 2;
+            this.BtnAceptar.Text = "Aceptar";
+            this.BtnAceptar.UseVisualStyleBackColor = true;
+            this.BtnAceptar.Click += new System.EventHandler(this.BtnAceptar_Click);
+            // 
+            // BtnCancelar
+            // 
+            this.BtnCancelar.Location = new System.Drawing.Point(239, 258);
+            this.BtnCancelar.Name = "BtnCancelar";
+            this.BtnCancelar.Size = new System.Drawing.Size(87, 27);
+            this.BtnCancelar.TabIndex = 3;
+            this.BtnCancelar.Text = "Cancelar";
+            this.BtnCancelar.UseVisualStyleBackColor = true;
+            this.BtnCancelar.Click += new System.EventHandler(this.BtnCancelar_Click);
+            // 
+            // VistaBaja
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(434, 297);
+            this.Controls.Add(this.BtnCancelar);
+            this.Controls.Add(this.BtnAceptar);
+            this.Controls.Add(this.LblMensaje);
+            this.Controls.Add(this.TxtMotivo);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "VistaBaja";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Confirmar Baja";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TxtMotivo;
+        private System.Windows.Forms.Label LblMensaje;
+        private System.Windows.Forms.Button BtnAceptar;
+        private System.Windows.Forms.Button BtnCancelar;
+    }
+}
